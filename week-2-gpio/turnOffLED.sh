c4d0d2209769b3496e53046bdea5e0c4a5ae213f
abdullah <abdullahyildiz@users.noreply.github.com>
2015-02-27 10:30:38 +0200

    Update turnOffLED.sh

--- week-2-gpio/turnOffLED.sh
+++ week-2-gpio/turnOffLED.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# GPIO1_12 refers to GPIO1 pin 12 which is the 12th pin on header P8 and used to drive the LED from BeagleBone Black
+
 # change directory to /sys/class/gpio
 cd /sys/class/gpio
 
@@ -9,5 +11,5 @@ cd gpio44
 # turn off the LED
 echo 0 > value
 
-# unexport pin 44 the userspace
+# unexport pin 44 from userspace
 echo 44 > /sys/class/gpio/unexport

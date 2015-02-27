125e42d6f0e8e849331770c058e047d5e7a33293
abdullah <abdullahyildiz@users.noreply.github.com>
2015-02-27 10:31:58 +0200

    Update and rename week-2-gpio to week-2-gpio/readSwitch.sh

---
+++ week-2-gpio/readSwitch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# GPIO1_13 refers to GPIO1 pin 13 which is the 11th pin on header P8 and used to read the status of the momentary push button from BeagleBone Black
+
+# change directory to /sys/class/gpio
+cd /sys/class/gpio
+
+# export pin number 45 to userspace
+echo 45 > export
+
+# change directory to gpio45
+cd gpio45
+
+# set the pin as an output
+echo in > direction
+
+cat value
+
+# unexport pin 45 from userspace
+echo 45 > /sys/class/gpio/unexport

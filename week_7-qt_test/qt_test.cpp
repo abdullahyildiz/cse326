f70ee4e3887e8d6b2b7cff5d5d929c9129097aff
abdullah <abdullah.mails@gmail.com>
2015-04-05 17:04:47 +0300

    added week_7_qt_test

---
+++ week_7-qt_test/qt_test.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[]){
+
+	QApplication app(argc, argv);
+	QLabel label("Hello CSE 326 ladies and gentlemen!");
+	label.resize(200,100);
+	label.show();
+
+	return app.exec();
+
+}

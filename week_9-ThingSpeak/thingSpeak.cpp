75fd5831cc98c8e31029cc9f9a64ace114b7b2f8
abdullah <abdullah.mails@gmail.com>
2015-05-10 12:47:38 +0300

    made minor changes

--- week_9-ThingSpeak/thingSpeak.cpp
+++ week_9-ThingSpeak/thingSpeak.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]){
    ostringstream head, data;
    cout << "Starting ThingSpeak Example" << endl;
    SocketClient sc("thingspeak.com",80);
-   data << "field1=" << getTemperature(readAnalog(0)) << endl;
+   data << "field1=" << argv[1] << endl;
    sc.connectToServer();
    head << "POST /update HTTP/1.1\n"
         << "Host: api.thingspeak.com\n"
